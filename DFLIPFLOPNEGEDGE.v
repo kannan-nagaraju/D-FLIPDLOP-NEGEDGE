f5805ea50cd15422adc89ddd4040c4f32a79a3b4
PRASHANTH <145742755+kannan-nagaraju@users.noreply.github.com>
2024-10-21 11:22:48 +0530

    Update DFLIPFLOPNEGEDGE.v

--- DFLIPFLOPNEGEDGE.v
+++ DFLIPFLOPNEGEDGE.v
@@ -1,6 +1,9 @@
 module DFLIPFLOPNEGEDGE(D,Clock,Q,Qbar);
 input D,Clock;
 output reg Q,Qbar;
-always @ // use negative edge clock for triggereing condition 
-//compute D flipflop logic here
- endmodule
+always @ (negedge Clock)
+  if(!reset)
+  Q <= 0;
+  else
+  Q <= D;
+endmodule
